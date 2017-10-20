bc554ff3d2467417182d65a37f3a0ac1042a094d
Dmitry Budaev <condemil@gmail.com>
2017-10-20 05:24:21 +0200

    Format file according to pep8

--- backup.py
+++ backup.py
@@ -2,7 +2,6 @@ from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 import zipfile
 import os
-import sys
 import datetime
 
 gauth = GoogleAuth()
@@ -10,24 +9,27 @@ gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 folders = []
 
-def zipFolder(foldername, target_dir):            
-    zipobj = zipfile.ZipFile(foldername + '.zip', 'w', zipfile.ZIP_DEFLATED)
+
+def zip_folder(folder_name, target_dir):
+    zipobj = zipfile.ZipFile(folder_name + '.zip', 'w', zipfile.ZIP_DEFLATED)
     rootlen = len(target_dir) + 1
     for base, dirs, files in os.walk(target_dir):
         for file in files:
             fn = os.path.join(base, file)
             zipobj.write(fn, fn[rootlen:])
 
-def uploadZip(target_dir):
-	file1 = drive.CreateFile({'title': 'test.zip'}) 
-	file1.SetContentFile(target_dir);
-	file1.Upload()
+
+def upload_zip(target_dir):
+    file1 = drive.CreateFile({'title': 'test.zip'})
+    file1.SetContentFile(target_dir);
+    file1.Upload()
+
 
 folders.append("F:\\Pictures\\logos")
 folders.append("F:\\Pictures\\Uplay")
 
 now = datetime.datetime.now()
 for f in folders:
-	zipname = str(os.path.basename(f)).replace(" ", "") + str(now.strftime("%Y-%m-%d-%H"))
-	zipFolder(zipname, f)	
-	uploadZip(zipname)
+    zipname = str(os.path.basename(f)).replace(" ", "") + str(now.strftime("%Y-%m-%d-%H"))
+    zip_folder(zipname, f)
+    upload_zip(zipname)
--- gui.py
+++ gui.py
@@ -3,29 +3,32 @@ from pages import *
 from cloudservices import *
 import threading
 
+
 # Google Cloud Option
-def onClick1():
-	p1.pack_forget()
-	p2.pack()
-	cloud = GDrive()
-	p2.pack_forget()
-	p3.pack()
+def on_click1():
+    p1.pack_forget()
+    p2.pack()
+    cloud = GDrive()
+    p2.pack_forget()
+    p3.pack()
+
 
 def thread_test(self):
-	t = threading.Thread(target=onClick1)
-	t.start()
+    t = threading.Thread(target=on_click1)
+    t.start()
+
 
 if __name__ == '__main__':
-	root = Tk()
-	root.title("Backup Software")
-	root.geometry("500x300")
-	root.configure(bg='white')
-	frame = Frame(root)
+    root = Tk()
+    root.title("Backup Software")
+    root.geometry("500x300")
+    root.configure(bg='white')
+    frame = Frame(root)
 
-	p1 = SelectPage(root)
-	p2 = LoadingPage(root)
-	p3 = MainPage(root)
+    p1 = SelectPage(root)
+    p2 = LoadingPage(root)
+    p3 = MainPage(root)
 
-	p1.gdrive.button.bind("<Button-1>", thread_test)
-	p1.pack()
-	root.mainloop()
+    p1.gdrive.button.bind("<Button-1>", thread_test)
+    p1.pack()
+    root.mainloop()
